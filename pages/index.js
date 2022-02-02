9b769400d3cffa60c9829b5b464e99f975bb767b
Lorena da Silva Azevedo <lorenaazevedo41@gmail.com>
2022-02-02 11:03:51 -0300

    adicionando arquivo _app e página de chat, roteamento e ajustes no index

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,7 @@
 import appConfig from "../config.json";
 import { Box, Button, Text, TextField, Image } from "@skynexui/components";
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: "Open Sans", sans-serif;
-      }
-      /* App fit Height */
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  );
-}
+import React, { useState } from "react";
+import { useRouter } from "next/router";
 
 function Title(props) {
   console.log(props);
@@ -64,11 +35,11 @@ function Title(props) {
 // export default HomePage;
 
 export default function PaginaInicial() {
-  const username = "peas";
+  const [username, setUsername] = useState("");
+  const roteamento = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: "flex",
@@ -102,6 +73,11 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function (event) {
+              event.preventDefault();
+              console.log("Submetido");
+              roteamento.push("/chat");
+            }}
             styleSheet={{
               display: "flex",
               flexDirection: "column",
@@ -122,8 +98,12 @@ export default function PaginaInicial() {
             >
               {appConfig.name}
             </Text>
-
             <TextField
+              value={username}
+              onChange={function (event) {
+                const username = event.target.value;
+                setUsername(username);
+              }}
               fullWidth
               textFieldColors={{
                 neutral: {
